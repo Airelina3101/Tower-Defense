c1d6037d3317ead790aa57c34c53e9c9ecca857e
Angelina <a-kargalceva@mail.ru>
2020-04-23 04:28:10 +0300

    Снова что-то потеряла. Добавляю

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject[] WayPoints;
+    public float SpeedEnemy = 1.0f;
+
+    private float _startToEndTravelTime;
+    private int _currentWayPoint = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WayPoints = GameObject.Find("PointsForEnemiesEast").transform;
+        _startToEndTravelTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPosition = WayPoints[_currentWayPoint].transform.position;
+        Vector3 endPosition = WayPoints[_currentWayPoint + 1].transform.position;
+        float pathLength = Vector3.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / SpeedEnemy;
+        float currentTimeOnPath = Time.time - _startToEndTravelTime;
+        gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+        if (gameObject.transform.position.Equals(endPosition))
+        {
+            if (_currentWayPoint < WayPoints.Length - 2)
+            {
+                _currentWayPoint++;
+                _startToEndTravelTime = Time.time;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject[] WayPoints;
+    public GameObject EnemyPrefab;
+    public float SpawnInterval = 2f;
+    public int CountEnemies = 10;
+
+    private float _timer;    
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if ((_timer < 0) && (--CountEnemies>-1))
+        {
+            Instantiate(EnemyPrefab).GetComponent<MoveEnemy>().WayPoints = WayPoints;
+
+            _timer = SpawnInterval;
+        }
+    }
+}
